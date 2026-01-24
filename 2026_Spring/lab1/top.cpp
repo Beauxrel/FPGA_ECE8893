5fc12d365071e6f3f19ee2de5d6373eb56516dea
Landon Jackson <landonbjackson@gmail.com>
2026-01-24 08:34:25 -0500

    - Change position of pragmas

--- 2026_Spring/lab1/top.cpp
+++ 2026_Spring/lab1/top.cpp
@@ -10,27 +10,24 @@ void top_kernel(data_t A[N_ROWS][N_COLS],
     #pragma HLS ARRAY_PARTITION variable=A   cyclic factor=16 dim=2
     #pragma HLS ARRAY_PARTITION variable=C   cyclic factor=16 dim=2
 
+    #pragma HLS loop_flatten
     // Phase 1: Row-wise normalization
     phase_1: for (int i = 0; i < N_ROWS; i++) {
-        #pragma HLS loop_flatten
         data_t row_sum = 0.0;
-
         // Compute row sum!
+        #pragma HLS PIPELINE II=1
+        #pragma HLS unroll factor=16
         compute_row: for (int j = 0; j < N_COLS; j++) {
-            #pragma HLS PIPELINE II=1
-            data_t v = A[i][j];
-            rowbuf[j] = v;
-            row_sum += (acc_t)v;
+            row_sum += A[i][j];
         }
-
         // Avoid division by zero, add small bias
-        data_t denom = row_sum + (data_t)1.0;
-
+        //data_t denom = row_sum + (data_t)1.0;
         // Normalize each element in the row
+        #pragma HLS PIPELINE II=1
+        #pragma HLS unroll factor=16
         norm_row: for (int j = 0; j < N_COLS; j++) {
-            #pragma HLS PIPELINE II=1
             #pragma HLS unroll factor=16
-            tmp[i][j] = (data_t)((acc_t)rowbuf[j] / denom);
+            tmp[i][j] = A[i][j] / row_sum + (data_t)1.0;
         }
     }
 
@@ -38,21 +35,18 @@ void top_kernel(data_t A[N_ROWS][N_COLS],
     for (int j = 0; j < N_COLS; j++) {
         #pragma HLS loop_flatten
         data_t col_sum = 0.0;
-
         // Compute column sum of normalized values
-        for (int i = 0; i < N_ROWS; i++) {
-            #pragma HLS PIPELINE II=1
+        #pragma HLS PIPELINE II=1
+        compute_col: for (int i = 0; i < N_ROWS; i++) {
             col_sum += tmp[i][j];
         }
-
         // Compute average as scale
-        data_t scale = col_sum / (data_t)N_ROWS;
-
+        //data_t scale = col_sum / (data_t)N_ROWS;
+        #pragma HLS PIPELINE II=1
+        #pragma HLS unroll factor=16
         // Apply scale to each element in the column
-        for (int i = 0; i < N_ROWS; i++) {
-            #pragma HLS PIPELINE II=1
-            #pragma HLS unroll factor=16
-            C[i][j] = tmp[i][j] * scale;
+        scale_col: for (int i = 0; i < N_ROWS; i++) {
+            C[i][j] = tmp[i][j] * col_sum / (data_t)N_ROWS;
         }
     }
 }
