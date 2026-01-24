d314e30494449e1b7acbb053941e5dce497cec20
Landon Jackson <landonbjackson@gmail.com>
2026-01-24 13:15:09 -0500

    - Move pipeline pragma

--- 2026_Spring/lab1/top.cpp
+++ 2026_Spring/lab1/top.cpp
@@ -12,9 +12,9 @@ void top_kernel(data_t A[N_ROWS][N_COLS],
 
     // Phase 1: Row-wise normalization
     phase_1: for (int i = 0; i < N_ROWS; i++) {
-        #pragma HLS pipeline
+        
         data_t row_sum = 0.0;
-
+        #pragma HLS pipeline
         // Compute row sum!
         compute_row: for (int j = 0; j < N_COLS; j++) {
             row_sum += A[i][j];
@@ -31,9 +31,9 @@ void top_kernel(data_t A[N_ROWS][N_COLS],
 
     // Phase 2: Column-wise scaling
     for (int j = 0; j < N_COLS; j++) {
-        #pragma HLS pipeline
+        
         data_t col_sum = 0.0;
-
+        #pragma HLS pipeline
         // Compute column sum of normalized values
         for (int i = 0; i < N_ROWS; i++) {
             col_sum += tmp[i][j];
