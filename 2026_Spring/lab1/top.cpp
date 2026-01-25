cb07cd8d386797f07b36052afc01ac128045ddcd
Landon Jackson <landonbjackson@gmail.com>
2026-01-25 10:38:02 -0500

    full unroll

--- 2026_Spring/lab1/top.cpp
+++ 2026_Spring/lab1/top.cpp
@@ -17,7 +17,7 @@ void top_kernel(data_t A[N_ROWS][N_COLS],
         // Compute row sum!
         compute_row: for (int j = 0; j < N_COLS; j++) {
 #pragma HLS PIPELINE II=1
-#pragma HLS unroll factor=5
+#pragma HLS unroll
             row_sum += A[i][j];
         }
 
@@ -27,7 +27,7 @@ void top_kernel(data_t A[N_ROWS][N_COLS],
         // Normalize each element in the row
         norm_row: for (int j = 0; j < N_COLS; j++) {
 #pragma HLS PIPELINE II=1
-#pragma HLS unroll factor=5
+#pragma HLS unroll
             tmp[i][j] = A[i][j] / denom;
         }
     }
@@ -39,7 +39,7 @@ void top_kernel(data_t A[N_ROWS][N_COLS],
         // Compute column sum of normalized values
         for (int i = 0; i < N_ROWS; i++) {
 #pragma HLS PIPELINE II=1
-#pragma HLS unroll factor=5
+#pragma HLS unroll
             col_sum += tmp[i][j];
         }
 
@@ -49,7 +49,7 @@ void top_kernel(data_t A[N_ROWS][N_COLS],
         // Apply scale to each element in the column
         for (int i = 0; i < N_ROWS; i++) {
 #pragma HLS PIPELINE II=1
-#pragma HLS unroll factor=5
+#pragma HLS unroll
             C[i][j] = tmp[i][j] * scale;
         }
     }
