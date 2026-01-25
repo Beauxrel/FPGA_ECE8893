40889f04adae18fc07a7977ef9378c22c5e7a309
Landon Jackson <landonbjackson@gmail.com>
2026-01-25 14:37:15 -0500

    unroll div by 8

--- 2026_Spring/lab1/top.cpp
+++ 2026_Spring/lab1/top.cpp
@@ -29,7 +29,7 @@ void top_kernel(data_t A[N_ROWS][N_COLS],
         // Normalize each element in the row
         norm_row: for (int j = 0; j < N_COLS; j++) {
 #pragma HLS PIPELINE II=1
-#pragma HLS unroll factor=4
+#pragma HLS unroll factor=8
             tmp[i][j] = A[i][j] / denom;
         }
     }
