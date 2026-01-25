46f48d9fa58238da37a68ccd5f5024ddc8eec1e5
Landon Jackson <landonbjackson@gmail.com>
2026-01-25 11:02:10 -0500

    skip_exit_check test

--- 2026_Spring/lab1/top.cpp
+++ 2026_Spring/lab1/top.cpp
@@ -6,9 +6,9 @@ void top_kernel(data_t A[N_ROWS][N_COLS],
                 data_t C[N_ROWS][N_COLS]) {
     // Intermediate buffer for row-normalized values
     static data_t tmp[N_ROWS][N_COLS];
-#pragma HLS ARRAY_PARTITION variable=tmp cyclic factor=64 dim=2
-#pragma HLS ARRAY_PARTITION variable=A   cyclic factor=64 dim=2
-#pragma HLS ARRAY_PARTITION variable=C   cyclic factor=64 dim=2
+#pragma HLS ARRAY_PARTITION variable=tmp cyclic factor=32 dim=2
+#pragma HLS ARRAY_PARTITION variable=A   cyclic factor=32 dim=2
+#pragma HLS ARRAY_PARTITION variable=C   cyclic factor=32 dim=2
 
     // Phase 1: Row-wise normalization
     phase_1: for (int i = 0; i < N_ROWS; i++) {
@@ -17,7 +17,7 @@ void top_kernel(data_t A[N_ROWS][N_COLS],
         // Compute row sum!
         compute_row: for (int j = 0; j < N_COLS; j++) {
 #pragma HLS PIPELINE II=1
-#pragma HLS unroll factor=4
+#pragma HLS unroll factor=4 skip_exit_check
             row_sum += A[i][j];
         }
 
@@ -27,7 +27,7 @@ void top_kernel(data_t A[N_ROWS][N_COLS],
         // Normalize each element in the row
         norm_row: for (int j = 0; j < N_COLS; j++) {
 #pragma HLS PIPELINE II=1
-#pragma HLS unroll factor=4
+#pragma HLS unroll factor=4 skip_exit_check
             tmp[i][j] = A[i][j] / denom;
         }
     }
@@ -39,7 +39,7 @@ void top_kernel(data_t A[N_ROWS][N_COLS],
         // Compute column sum of normalized values
         for (int i = 0; i < N_ROWS; i++) {
 #pragma HLS PIPELINE II=1
-#pragma HLS unroll factor=4
+#pragma HLS unroll factor=4 skip_exit_check
             col_sum += tmp[i][j];
         }
 
@@ -49,7 +49,7 @@ void top_kernel(data_t A[N_ROWS][N_COLS],
         // Apply scale to each element in the column
         for (int i = 0; i < N_ROWS; i++) {
 #pragma HLS PIPELINE II=1
-#pragma HLS unroll factor=4
+#pragma HLS unroll factor=4 skip_exit_check
             C[i][j] = tmp[i][j] * scale;
         }
     }
