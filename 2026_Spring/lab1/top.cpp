e97d7765096156d023fa4e68f2f3897f1e78cc57
Landon Jackson <landonbjackson@gmail.com>
2026-01-30 18:49:23 -0500

    .

--- 2026_Spring/lab1/top.cpp
+++ 2026_Spring/lab1/top.cpp
@@ -1,55 +1,56 @@
 #include "dcl.h"
 
+// Baseline implementation for HLS.
+// Students will optimize this (loops, memory access, etc.).
 void top_kernel(data_t A_DRAM[N_ROWS][N_COLS],
                 data_t C_DRAM[N_ROWS][N_COLS])
 {
-#pragma HLS interface m_axi port = A_DRAM offset = slave bundle = A \
-    max_read_burst_length=256 num_read_outstanding=16 latency=120
-#pragma HLS interface m_axi port = C_DRAM offset = slave bundle = C \
-    max_write_burst_length=256 num_write_outstanding=16 latency=120
+#pragma HLS interface m_axi port = A_DRAM offset = slave bundle = A
+#pragma HLS interface m_axi port = C_DRAM offset = slave bundle = C
 #pragma HLS interface s_axilite port = return
 
-    // On-chip buffers
+    // On-chip buffers for A_DRAM and C_DRAM
     data_t A[N_ROWS][N_COLS];
     data_t C[N_ROWS][N_COLS];
-    data_t tmp[N_ROWS][N_COLS];
-    
-#pragma HLS ARRAY_PARTITION variable = A cyclic factor = 32 dim = 2
-#pragma HLS ARRAY_PARTITION variable = C cyclic factor = 32 dim = 2
-#pragma HLS ARRAY_PARTITION variable = tmp cyclic factor = 32 dim = 2
 
-    // Load from DRAM
+#pragma HLS ARRAY_PARTITION variable = A cyclic factor = 32 dim = 1
+#pragma HLS ARRAY_PARTITION variable = C cyclic factor = 32 dim = 2
+
 dram_to_bram_outer:
     for (int i = 0; i < N_ROWS; i++)
     {
-dram_to_bram_inner:
+    dram_to_bram_inner:
         for (int j = 0; j < N_COLS; j++)
         {
-#pragma HLS PIPELINE II = 1
+#pragma HLS LOOP_FLATTEN
             A[i][j] = A_DRAM[i][j];
         }
     }
 
+    // Intermediate buffer for row-normalized values
+    data_t tmp[N_ROWS][N_COLS];
+#pragma HLS bind_storage variable = tmp type = fifo
+#pragma HLS ARRAY_PARTITION variable = tmp complete
     // Phase 1: Row-wise normalization
 phase_1:
     for (int i = 0; i < N_ROWS; i++)
     {
         data_t row_sum = 0.0;
-        
+#pragma HLS PIPELINE II = 1
         // Compute row sum
-compute_row_sum:
+    compute_row:
         for (int j = 0; j < N_COLS; j++)
         {
-#pragma HLS UNROLL
+#pragma HLS UNROLL factor = 8
             row_sum += A[i][j];
         }
-        
-        // Normalize row
+
+        // Avoid division by zero, add small bias
         data_t denom = row_sum + (data_t)1.0;
-normalize_row:
+    div_loop:
         for (int j = 0; j < N_COLS; j++)
         {
-#pragma HLS UNROLL
+#pragma HLS UNROLL factor = 8
             tmp[i][j] = A[i][j] / denom;
         }
     }
@@ -58,34 +59,44 @@ normalize_row:
 phase_2:
     for (int j = 0; j < N_COLS; j++)
     {
-        data_t col_sum_val = 0.0;  // Fixed: renamed variable
-        
-        // Compute column sum
-compute_col_sum:
+        data_t col_sum = 0.0;
+#pragma HLS PIPELINE II = 1
+        // Compute column sum of normalized values
+    col_sum:
         for (int i = 0; i < N_ROWS; i++)
         {
-#pragma HLS UNROLL
-            col_sum_val += tmp[i][j];
+#pragma HLS UNROLL factor = 8
+            col_sum += tmp[i][j];
         }
-        
-        // Apply scaling
-        data_t scale = col_sum_val / (data_t)N_ROWS;
-col_scaling:
+
+        // Compute average as scale
+        data_t scale = col_sum / (data_t)N_ROWS;
+
+        // Apply scale to each element in the column
+    col_scaling:
         for (int i = 0; i < N_ROWS; i++)
         {
-#pragma HLS UNROLL
+#pragma HLS UNROLL factor = 8
             C[i][j] = tmp[i][j] * scale;
         }
     }
 
-    // Write back to DRAM (removed duplicate)
+    for (int i = 0; i < N_ROWS; i++)
+    {
+        for (int j = 0; j < N_COLS; j++)
+        {
+#pragma HLS UNROLL factor = 8
+            C_DRAM[i][j] = C[i][j];
+        }
+    }
+
 bram_to_dram_outer:
     for (int i = 0; i < N_ROWS; i++)
     {
-bram_to_dram_inner:
+    bram_to_dram_inner:
         for (int j = 0; j < N_COLS; j++)
         {
-#pragma HLS PIPELINE II = 1
+#pragma HLS LOOP_FLATTEN
             C_DRAM[i][j] = C[i][j];
         }
     }
