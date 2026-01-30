b8a28de0dd3479b98eae02cfa6c4ee1dd714a073
Landon Jackson <landonbjackson@gmail.com>
2026-01-30 06:34:33 -0500

    .

--- 2026_Spring/lab1/top.cpp
+++ 2026_Spring/lab1/top.cpp
@@ -3,28 +3,33 @@
 // Baseline implementation for HLS.
 // Students will optimize this (loops, memory access, etc.).
 void top_kernel(data_t A_DRAM[N_ROWS][N_COLS],
-                data_t C_DRAM[N_ROWS][N_COLS]) {
+                data_t C_DRAM[N_ROWS][N_COLS])
+{
     // Intermediate buffer for row-normalized values
     data_t tmp[N_ROWS][N_COLS];
     // On-chip buffers for A_DRAM and C_DRAM
     data_t A[N_ROWS][N_COLS];
     data_t C[N_ROWS][N_COLS];
     data_t rowbuf[N_COLS];
-#pragma HLS ARRAY_PARTITION variable=rowbuf cyclic factor=32 dim=1
-#pragma HLS interface m_axi port=A_DRAM offset=slave bundle=A max_widen_bitwidth=512
-#pragma HLS interface m_axi port=C_DRAM offset=slave bundle=C max_widen_bitwidth=512
-#pragma HLS interface s_axilite port=return
-#pragma HLS ARRAY_PARTITION variable=tmp cyclic factor=32 dim=1
-#pragma HLS ARRAY_PARTITION variable=A   cyclic factor=32 dim=2
-#pragma HLS ARRAY_PARTITION variable=C   cyclic factor=32 dim=1
+#pragma HLS ARRAY_PARTITION variable = rowbuf cyclic factor = 32 dim = 1
+#pragma HLS interface m_axi port = A_DRAM offset = slave bundle = A max_widen_bitwidth = 512
+#pragma HLS interface m_axi port = C_DRAM offset = slave bundle = C max_widen_bitwidth = 512
+#pragma HLS interface s_axilite port = return
+#pragma HLS ARRAY_PARTITION variable = tmp cyclic factor = 64 dim = 1
+#pragma HLS ARRAY_PARTITION variable = A cyclic factor = 32 dim = 2
+#pragma HLS ARRAY_PARTITION variable = C cyclic factor = 32 dim = 1
 
-    // Phase 1: Row-wise normalization
-    phase_1: for (int i = 0; i < N_ROWS; i++) {
+// Phase 1: Row-wise normalization
+phase_1:
+    for (int i = 0; i < N_ROWS; i++)
+    {
         data_t row_sum = 0.0;
-        // Compute row sum!
-        compute_row: for (int j = 0; j < N_COLS; j++) {
-#pragma HLS PIPELINE II=1
-#pragma HLS unroll factor=4
+    // Compute row sum!
+    compute_row:
+        for (int j = 0; j < N_COLS; j++)
+        {
+#pragma HLS PIPELINE II = 1
+#pragma HLS unroll factor = 4
             rowbuf[j] = A_DRAM[i][j];
             row_sum += rowbuf[j];
         }
@@ -32,22 +37,26 @@ void top_kernel(data_t A_DRAM[N_ROWS][N_COLS],
         // Avoid division by zero, add small bias
         data_t denom = row_sum + (data_t)1.0;
 
-        // Normalize each element in the row
-        norm_row: for (int j = 0; j < N_COLS; j++) {
-#pragma HLS PIPELINE II=1
-#pragma HLS unroll factor=4
+    // Normalize each element in the row
+    norm_row:
+        for (int j = 0; j < N_COLS; j++)
+        {
+#pragma HLS PIPELINE II = 1
+#pragma HLS unroll factor = 4
             tmp[i][j] = rowbuf[j] / denom;
         }
     }
 
     // Phase 2: Column-wise scaling
-    for (int j = 0; j < N_COLS; j++) {
+    for (int j = 0; j < N_COLS; j++)
+    {
         data_t col_sum = 0.0;
 
         // Compute column sum of normalized values
-        for (int i = 0; i < N_ROWS; i++) {
-#pragma HLS PIPELINE II=1
-#pragma HLS unroll factor=4
+        for (int i = 0; i < N_ROWS; i++)
+        {
+#pragma HLS PIPELINE II = 1
+#pragma HLS unroll factor = 4
             col_sum += tmp[i][j];
         }
 
@@ -55,16 +64,19 @@ void top_kernel(data_t A_DRAM[N_ROWS][N_COLS],
         data_t scale = col_sum / (data_t)N_ROWS;
 
         // Apply scale to each element in the column
-        for (int i = 0; i < N_ROWS; i++) {
-#pragma HLS PIPELINE II=1
-#pragma HLS unroll factor=4
+        for (int i = 0; i < N_ROWS; i++)
+        {
+#pragma HLS PIPELINE II = 1
+#pragma HLS unroll factor = 4
             C[i][j] = tmp[i][j] * scale;
         }
     }
-    for (int i = 0; i < N_ROWS; i++) {
-        for (int j = 0; j < N_COLS; j++) {
-#pragma HLS PIPELINE II=1
-#pragma HLS unroll factor=4
+    for (int i = 0; i < N_ROWS; i++)
+    {
+        for (int j = 0; j < N_COLS; j++)
+        {
+#pragma HLS PIPELINE II = 1
+#pragma HLS unroll factor = 4
             C_DRAM[i][j] = C[i][j];
         }
     }
