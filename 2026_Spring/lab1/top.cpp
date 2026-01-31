e540194de32dd147d1d439aafb50cabc80bcb3d5
Landon Jackson <landonbjackson@gmail.com>
2026-01-30 19:41:00 -0500

    .

--- 2026_Spring/lab1/top.cpp
+++ 2026_Spring/lab1/top.cpp
@@ -5,8 +5,8 @@
 void top_kernel(data_t A_DRAM[N_ROWS][N_COLS],
                 data_t C_DRAM[N_ROWS][N_COLS])
 {
-#pragma HLS interface m_axi port = A_DRAM offset = slave bundle = A
-#pragma HLS interface m_axi port = C_DRAM offset = slave bundle = C
+#pragma HLS interface m_axi port = A_DRAM offset = direct bundle = A
+#pragma HLS interface m_axi port = C_DRAM offset = direct bundle = C
 #pragma HLS interface s_axilite port = return
 
     // On-chip buffers for A_DRAM and C_DRAM
@@ -22,7 +22,6 @@ dram_to_bram_outer:
     dram_to_bram_inner:
         for (int j = 0; j < N_COLS; j++)
         {
-#pragma HLS PIPELINE II = 1
             A[i][j] = A_DRAM[i][j];
         }
     }
@@ -96,7 +95,6 @@ bram_to_dram_outer:
     bram_to_dram_inner:
         for (int j = 0; j < N_COLS; j++)
         {
-#pragma HLS PIPELINE II = 1
             C_DRAM[i][j] = C[i][j];
         }
     }
