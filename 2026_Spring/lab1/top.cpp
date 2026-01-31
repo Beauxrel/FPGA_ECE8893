125523bc24aedb09301f56877a4fd746fd0d9f3a
Landon Jackson <landonbjackson@gmail.com>
2026-01-31 18:25:56 -0500

    .

--- 2026_Spring/lab1/top.cpp
+++ 2026_Spring/lab1/top.cpp
@@ -5,13 +5,15 @@
 void top_kernel(data_t A_DRAM[N_ROWS][N_COLS],
                 data_t C_DRAM[N_ROWS][N_COLS])
 {
-#pragma HLS interface mode=axis port = A_DRAM offset = slave bundle = A max_widen_bitwidth=512 max_read_burst_length=64
-#pragma HLS interface mode=axis port = C_DRAM offset = slave bundle = C max_widen_bitwidth=512 max_read_burst_length=64
+#pragma HLS interface mode=m_axi port = A_DRAM offset = slave bundle = A max_widen_bitwidth=512 max_read_burst_length=64
+#pragma HLS interface mode=m_axi port = C_DRAM offset = slave bundle = C max_widen_bitwidth=512 max_read_burst_length=64
 #pragma HLS interface s_axilite port = return
 
     // On-chip buffers for A_DRAM and C_DRAM
     data_t A[N_ROWS][N_COLS];
     data_t C[N_ROWS][N_COLS];
+    data_t row_sum = 0.0;
+    data_t col_sum = 0.0;
 
 #pragma HLS ARRAY_PARTITION variable = A cyclic factor = 32 dim = 1
 #pragma HLS ARRAY_PARTITION variable = C cyclic factor = 32 dim = 2
@@ -34,8 +36,7 @@ dram_to_bram_outer:
 phase_1:
     for (int i = 0; i < N_ROWS; i++)
     {
-#pragma HLS UNROLL factor = 4
-        data_t row_sum = 0.0;
+        row_sum = 0.0;
         // Compute row sum
     compute_row:
         for (int j = 0; j < N_COLS; j++)
@@ -43,7 +44,10 @@ phase_1:
 #pragma HLS PIPELINE II = 1
             row_sum += A[i][j];
         }
-
+    }
+phase_2:
+    for (int i = 0; i < N_ROWS; i++)
+    {
         // Avoid division by zero, add small bias
         data_t denom = row_sum + (data_t)1.0;
     div_loop:
@@ -53,13 +57,12 @@ phase_1:
             tmp[i][j] = A[i][j] / denom;
         }
     }
-
+phase_3:
     // Phase 2: Column-wise scaling
-phase_2:
+
     for (int j = 0; j < N_COLS; j++)
     {
-#pragma HLS UNROLL factor = 4
-        data_t col_sum = 0.0;
+        col_sum = 0.0;
         // Compute column sum of normalized values
     col_sum:
         for (int i = 0; i < N_ROWS; i++)
@@ -67,7 +70,10 @@ phase_2:
 #pragma HLS PIPELINE II = 1
             col_sum += tmp[i][j];
         }
-
+    }
+phase_4:
+    for (int j = 0; j < N_COLS; j++)
+    {
         // Compute average as scale
         data_t scale = col_sum / (data_t)N_ROWS;
 
@@ -77,15 +83,6 @@ phase_2:
         {
 #pragma HLS PIPELINE II = 1
             C[i][j] = tmp[i][j] * scale;
-        }
-    }
-
-    for (int i = 0; i < N_ROWS; i++)
-    {
-        for (int j = 0; j < N_COLS; j++)
-        {
-#pragma HLS PIPELINE II = 1
-            C_DRAM[i][j] = C[i][j];
         }
     }
 
