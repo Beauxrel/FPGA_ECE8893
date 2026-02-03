4f1d30008aa0603ff892a719c05148a9c88b39d0
Landon Jackson <landonbjackson@gmail.com>
2026-02-02 21:40:42 -0500

    .

--- 2026_Spring/lab1/top.cpp
+++ 2026_Spring/lab1/top.cpp
@@ -61,7 +61,7 @@ phase_1:
         compute_row:
             for (int tj = 0; tj < TILE_COLS; tj++) {
 #pragma HLS PIPELINE II=1
-#pragma HLS UNROLL factor=4
+#pragma HLS UNROLL factor=8
                 int j = jb + tj;
                 if (j < N_COLS) {
                     acc += A[i][j];
@@ -81,8 +81,8 @@ phase_2:
         for (int jb = 0; jb < N_COLS; jb += TILE_COLS) {
         div_loop:
             for (int tj = 0; tj < TILE_COLS; tj++) {
-#pragma HLS PIPELINE II=4
-#pragma HLS UNROLL factor=4
+#pragma HLS PIPELINE II=1
+#pragma HLS UNROLL factor=8
                 int j = jb + tj;
                 if (j < N_COLS) {
                     tmp[i][j] = A[i][j] / denom;
@@ -140,7 +140,7 @@ phase_4:
     for (int i = 0; i < N_ROWS; i++) {
         for (int jb = 0; jb < N_COLS; jb += TILE_COLS) {
             for (int tj = 0; tj < TILE_COLS; tj++) {
-#pragma HLS PIPELINE II=4
+#pragma HLS PIPELINE II=1
 #pragma HLS UNROLL factor=4
                 int j = jb + tj;
                 if (j < N_COLS) {
