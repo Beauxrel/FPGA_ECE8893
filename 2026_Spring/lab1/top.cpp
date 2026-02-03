bafd1c77e0dd8ceb77505659e21b7313c441b012
Landon Jackson <landonbjackson@gmail.com>
2026-02-02 19:29:44 -0500

    .

--- 2026_Spring/lab1/top.cpp
+++ 2026_Spring/lab1/top.cpp
@@ -17,9 +17,9 @@ void top_kernel(data_t A_DRAM[N_ROWS][N_COLS],
     // Intermediate buffer for row-normalized values
     data_t tmp[N_ROWS][N_COLS];
 
-#pragma HLS ARRAY_PARTITION variable=A   cyclic factor=32 dim=2
-#pragma HLS ARRAY_PARTITION variable=tmp cyclic factor=32 dim=2
-#pragma HLS ARRAY_PARTITION variable=C   cyclic factor=32 dim=2
+#pragma HLS ARRAY_PARTITION variable=A   cyclic factor=16 dim=2
+#pragma HLS ARRAY_PARTITION variable=tmp cyclic factor=16 dim=2
+#pragma HLS ARRAY_PARTITION variable=C   cyclic factor=16 dim=2
 
     // Read in the data from DRAM to BRAM (tiled over columns)
     for (int i = 0; i < N_ROWS; i++) {
