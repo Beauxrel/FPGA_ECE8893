40335a9eb9fe516fb1577b688cdf5bc0c84b228e
Landon Jackson <landonbjackson@gmail.com>
2026-02-07 22:05:38 -0500

    .

--- 2026_Spring/lab1/top.cpp
+++ 2026_Spring/lab1/top.cpp
@@ -19,7 +19,7 @@ void top_kernel(data_t A_DRAM[N_ROWS][N_COLS],
 #pragma HLS ARRAY_PARTITION variable = tmp cyclic factor = 8 dim = 1
 #pragma HLS ARRAY_PARTITION variable = A cyclic factor = 10 dim = 2
 #pragma HLS ARRAY_PARTITION variable = C cyclic factor = 10 dim = 2
-#pragma HLS ARRAY_PARTITION variable = row_sum cyclic factor = 10 dim = 1
+#pragma HLS ARRAY_PARTITION variable = row_sum cyclic factor = 16 dim = 1
 #pragma HLS ARRAY_PARTITION variable = col_sum cyclic factor = 10 dim = 1
 
 dram_to_bram_outer: for (int i = 0; i < N_ROWS; i++){
@@ -34,9 +34,9 @@ dram_to_bram_inner:     for (int j = 0; j < N_COLS; j++){
 phase_1:    for (int i = 0; i < N_ROWS; i++){
         // Compute row sum
                 row_sum[i] = 0;
-#pragma HLS PIPELINE II=1 
+#pragma HLS unroll factor=16
 compute_row:    for (int j = 0; j < N_COLS; j++){
-#pragma HLS unroll factor=10
+#pragma HLS PIPELINE II=1
                     row_sum[i] += A[i][j];
                 }
             }
