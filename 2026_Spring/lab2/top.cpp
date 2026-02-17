f46e53a68e03b4090874466dc758ddbe710cfc29
Landon Jackson <landonbjackson@gmail.com>
2026-02-17 11:18:13 -0500

    .

--- 2026_Spring/lab2/top.cpp
+++ 2026_Spring/lab2/top.cpp
@@ -22,8 +22,8 @@ void top_kernel(const data_t A_in[NX][NY],
     const data_t wa = (data_t)0.10;
     const data_t wd = (data_t)0.025;
 
-#pragma HLS array_partition variable=cur dim=2 type=cyclic factor=32
-#pragma HLS array_partition variable=nxt dim=2 type=cyclic factor=32
+#pragma HLS array_partition variable=cur dim=2 type=cyclic factor=8
+#pragma HLS array_partition variable=nxt dim=2 type=cyclic factor=8
 
     // Copy input into cur
     for (int i = 0; i < NX; i++) {
@@ -37,12 +37,10 @@ void top_kernel(const data_t A_in[NX][NY],
     for (int t = 0; t < TSTEPS; t++) {
         // Copy boundaries unchanged
         for (int j = 0; j < NY; j++) {
-#pragma HLS unroll factor=4
             nxt[0][j]      = cur[0][j];
             nxt[NX - 1][j] = cur[NX - 1][j];
         }
         for (int i = 0; i < NX; i++) {
-#pragma HLS unroll factor=4
             nxt[i][0]      = cur[i][0];
             nxt[i][NY - 1] = cur[i][NY - 1];
         }
