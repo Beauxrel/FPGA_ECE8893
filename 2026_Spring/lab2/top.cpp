9a640c3f777c61f3eaa03196b2469ad52eaed779
Landon Jackson <landonbjackson@gmail.com>
2026-02-22 23:34:20 -0500

    I hate myself

--- 2026_Spring/lab2/top.cpp
+++ 2026_Spring/lab2/top.cpp
@@ -2,33 +2,31 @@
 
 static data_t buf0[NX][NY];
 static data_t buf1[NX][NY];
-#pragma HLS array_partition variable = buf0 cyclic factor = 16 dim = 2
-#pragma HLS array_partition variable = buf1 cyclic factor = 16 dim = 2
 
 static data_t row_prev[NY];
 static data_t row_curr[NY];
 static data_t row_next[NY];
-#pragma HLS array_partition variable = row_prev cyclic factor = 16 dim = 1
-#pragma HLS array_partition variable = row_curr cyclic factor = 16 dim = 1
-#pragma HLS array_partition variable = row_next cyclic factor = 16 dim = 1
 
 static void stencil_pass(data_t rd[NX][NY], data_t wr[NX][NY])
 {
 #pragma HLS inline off
+#pragma HLS array_partition variable = rd cyclic factor = 16 dim = 2
+#pragma HLS array_partition variable = wr cyclic factor = 16 dim = 2
+#pragma HLS array_partition variable = row_prev cyclic factor = 16 dim = 1
+#pragma HLS array_partition variable = row_curr cyclic factor = 16 dim = 1
+#pragma HLS array_partition variable = row_next cyclic factor = 16 dim = 1
+
     const acc_t wc = (acc_t)0.50;
     const acc_t wa = (acc_t)0.10;
     const acc_t wd = (acc_t)0.025;
 
-// Preload: prev=row0, curr=row0, next=row1
-// So that when i=0 runs, curr=row0 and next=row1 are correct.
-// prev is unused for i=0 (boundary), so it can be anything.
 PRELOAD:
     for (int j = 0; j < NY; j++)
     {
 #pragma HLS pipeline II = 1
-        row_prev[j] = rd[0][j]; // unused for i=0 boundary, but safe
-        row_curr[j] = rd[0][j]; // correct current row for i=0
-        row_next[j] = rd[1][j]; // correct next row for i=0
+        row_prev[j] = rd[0][j];
+        row_curr[j] = rd[0][j];
+        row_next[j] = rd[1][j];
     }
 
 STENCIL_I:
@@ -36,7 +34,6 @@ STENCIL_I:
     {
 #pragma HLS loop_tripcount min = NX max = NX
 
-    // Apply stencil FIRST using current cache state
     STENCIL_J:
         for (int j = 0; j < NY; j++)
         {
@@ -63,8 +60,6 @@ STENCIL_I:
             }
         }
 
-        // THEN rotate cache to prepare for i+1
-        // next row to load is i+2, clamped to NX-1
         int next_row = (i + 2 < NX) ? (i + 2) : (NX - 1);
     ROTATE:
         for (int j = 0; j < NY; j++)
@@ -82,6 +77,8 @@ void top_kernel(const data_t A_in[NX][NY], data_t A_out[NX][NY])
 #pragma HLS interface m_axi port = A_in offset = slave bundle = gmem0 depth = 16384
 #pragma HLS interface m_axi port = A_out offset = slave bundle = gmem1 depth = 16384
 #pragma HLS interface s_axilite port = return
+#pragma HLS array_partition variable = buf0 cyclic factor = 16 dim = 2
+#pragma HLS array_partition variable = buf1 cyclic factor = 16 dim = 2
 
 INIT_I:
     for (int i = 0; i < NX; i++)
